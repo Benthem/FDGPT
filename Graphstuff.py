d02357e46646ef00bbd6d626899eaf9be51644e8
Yoeri Poels <yoeri.poels@hotmail.com>
2019-02-17 17:07:55 +0100

    Some interaction with tree, click on a node to set it as focal point, right click to go back 1 step. Simple linear interpolation for translating the focus.

--- Graphstuff.py
+++ Graphstuff.py
@@ -1,3 +1,4 @@
+import math
 from math import pi
 
 
@@ -19,19 +20,36 @@ class Node(object):
 
 
 class Rectangle(object):
+
     def __init__(self, c, x, y, t):
         self.c = c
         self.x = x
         self.y = y
         self.t = t
 
-    def draw(self, arcade):
+    def offsetcenter(self, dx, dy, dt, rc, zoom):
+        #print('Old values for rect (' + str(self.c[0]) + ', ' + str(self.c[1]) + ')')
+        # rotate around rc
+        cx = self.c[0] - rc[0]
+        cy = self.c[1] - rc[1]
+        newx = cx * math.cos(dt) - cy * math.sin(dt)
+        newy = cx * math.sin(dt) + cy * math.cos(dt)
+        # translate back and apply dx, dy
+        newx *= zoom
+        newy *= zoom
+        newx += dx + rc[0]
+        newy += dy + rc[1]
+        #print('New values for rect (' + str(newx) + ', ' + str(newy) + ')')
+
+        return (newx, newy)
+
+    def draw(self, arcade, dx, dy, dt, rx, ry, zoom):
         arcade.draw_rectangle_filled(
-                *self.c, 
-                self.x,
-                self.y, 
+            *self.offsetcenter(dx, dy, dt, (rx, ry), zoom),
+                self.x * zoom,
+                self.y * zoom,
                 color=(255, 0, 0, 100),
-                tilt_angle=(pi/2 - self.t)*180/3.141592)
+                tilt_angle=(pi/2 - self.t + dt)*180/3.141592)
 
     def __str__(self):
         return "c%d,%d x%d y%d t%f" % (*self.c, self.x, self.y, self.t)
--- treevis.py
+++ treevis.py
@@ -1,11 +1,22 @@
+import math
+import random
+
 import Graphstuff as G
 import arcade
 from math import sin, cos, pi
 
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 1000
+SCREEN_WIDTH = 1000
 SCREEN_TITLE = "500"
 
+def treeToList(root):
+    newnodes = [root.data]
+    if not root.children:
+        return newnodes
+    else:
+        for child in root.children:
+            newnodes.extend(treeToList(child))
+        return newnodes
 
 def generalizedPythagorasTree(H):
     R = H.data
@@ -28,12 +39,12 @@ def generalizedPythagorasTree(H):
         generalizedPythagorasTree(n)
 
 
-def drawGPT(H):
-    H.data.draw(arcade)
+def drawGPT(H, focus):
+    H.data.draw(arcade, focus[0], focus[1], focus[2], focus[3], focus[4], focus[5])
     if not H.children: 
         return
     for n in H.children:
-        drawGPT(n)
+        drawGPT(n, focus)
 
 
 def computeCenter(Rc, Rx, Ry, Rt, a, x, y):
@@ -52,6 +63,20 @@ def computeSlope(Rt, a):
 
 
 class MyGame(arcade.Window):
+    def interpolate(self, deltatime):
+        # don't interpolate if finished
+        if self.interpolationcounter >= self.interpolationtime:
+            return
+        # only interpolate if we have a start/end point (and status)
+        if len(self.startfocus) == 0 or len(self.endfocus) == 0 or len(self.focus) == 0:
+            return
+        for i in range(0, len(self.focus)):
+            self.focus[i] += (self.endfocus[i] - self.startfocus[i])/self.interpolationtime
+        self.interpolationcounter += 1
+        # done interpolating
+        if self.interpolationcounter == self.interpolationtime:
+            self.startfocus = self.focus[:]
+
     def __init__(self, width, height, title, root):
         # Call the parent class's init function
         super().__init__(width, height, title)
@@ -63,15 +88,79 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.DUTCH_WHITE)
 
         self.root = root
+        self.nodelist = treeToList(self.root)
+        print(self.nodelist)
+        self.r = 0
+
+        self.startfocus = []
+        self.endfocus = []
+        self.focus = []
+        self.interpolationtime = 60
+        self.interpolationcounter = 0
+        arcade.schedule(self.interpolate, 1/60)
+        self.focusrect = self.nodelist[0]
+        self.focus = self.setfocus(self.focusrect)
+        self.startfocus = self.focus[:]
+        self.focusstack = []
+
+
+    # the offset for drawing, when focused on rect
+    def setfocus(self, rect):
+        # zoom constant, 0.5 means 50% of screen width must be covered by focused rectangle width
+        zoomc = 0.2
+        yoffset = SCREEN_HEIGHT/4
+        focus = [-rect.c[0] + SCREEN_WIDTH/2, -rect.c[1] + SCREEN_HEIGHT/2 - yoffset, rect.t, rect.c[0], rect.c[1], SCREEN_WIDTH/rect.x * zoomc]
+        return focus
+
+    def focus_on(self, rect):
+        self.startfocus = self.focus[:]
+        self.endfocus = self.setfocus(rect)
+        self.interpolationcounter = 0
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_RIGHT:
+            if len(self.focusstack) > 0:
+                self.focusrect = self.focusstack.pop()
+                self.focus_on(self.focusrect)
+
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            # apply translation for focus in reverse
+            newx = x - self.focus[0] - self.focus[3]
+            newy = y - self.focus[1] - self.focus[4]
+            newx /= self.focus[5]
+            newy /= self.focus[5]
+            oldx = newx * cos(-1*self.focus[2]) - newy * sin(-1*self.focus[2])
+            oldy = newx * sin(-1*self.focus[2]) + newy * cos(-1*self.focus[2])
+            oldx += self.focus[3]
+            oldy += self.focus[4]
+
+            clicked = None
+            for rect in self.nodelist:
+                # rotate locally per rect to check if it falls inside
+                nx = oldx - rect.c[0]
+                ny = oldy - rect.c[1]
+                rx = nx * cos(rect.t) - ny * sin(rect.t)
+                ry = nx * sin(rect.t) + ny * cos(rect.t)
+                rx += rect.c[0]
+                ry += rect.c[1]
+                if math.fabs(rx - rect.c[0]) < rect.x/2 and math.fabs(ry - rect.c[1]) < rect.y/2:
+                    clicked = rect
+
+            if clicked is not None:
+                # add previous to stack to go back to
+                self.focusstack.append(self.focusrect)
+                self.focusrect = clicked
+                self.focus_on(self.focusrect)
+
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
-        drawGPT(self.root)
+        drawGPT(self.root, self.focus)
 
 
 def main():
-    with open('filetree.in', 'r') as f:
+    with open('sometree.in', 'r') as f:
         s = f.read().split('\n')
         n = int(s.pop(0))
         nodes = [G.Node(i) for i in range(n)]
@@ -83,7 +172,7 @@ def main():
             for j in range(int(l.pop(0))):
                 nodes[i].addChild(nodes[int(l.pop(0))])
 
-    root.data = G.Rectangle((250, 100), 100, 100, 0)
+        root.data = G.Rectangle((250, 100), 100, 100, 0)
     generalizedPythagorasTree(root)
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, root)
     arcade.run()
